29a528cc99e9510109dd288a15922bf458690854
Gustavo Gomes <gustavosgdev@outlook.com>
2024-07-07 23:14:37 -0400

    jsconfig.json

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,7 +1,6 @@
-import database from "../../../../infra/database.js";
+import database from "infra/database.js";
 
 export default async function status(request, response) {
   const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result.rows);
   response.status(200).json({ message: "Hello, world!" });
 }
